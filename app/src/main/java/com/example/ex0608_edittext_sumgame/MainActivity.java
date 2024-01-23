72fc5e85aa9270e913b3283f290f14480e7cf2b3
ml3424 <ml3424@bs.amalnet.k12.il>
2024-01-23 09:27:28 +0200

    second line

--- app/src/main/java/com/example/ex0608_edittext_sumgame/MainActivity.java
+++ app/src/main/java/com/example/ex0608_edittext_sumgame/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.ex0608_edittext_sumgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tV1, tV2, tV3, tV4, tV5, tV6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tV1 =  findViewById(R.id.tV1);
+        tV2 =  findViewById(R.id.tV2);
+        tV3 =  findViewById(R.id.tV3);
+        tV4 =  findViewById(R.id.tV4);
+        tV5 =  findViewById(R.id.tV5);
+        tV6 =  findViewById(R.id.tV6);
     }
 }
